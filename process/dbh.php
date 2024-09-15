0b38362c22d79e962ebd16424051d3cdb58b812b
Abubakar-88 <76661821+Abubakar-88@users.noreply.github.com>
2024-09-15 21:59:57 +0600

    Update dbh.php

--- process/dbh.php
+++ process/dbh.php
@@ -4,11 +4,9 @@ $servername = "srv995.hstgr.io";
 $dBUsername = "u611910731_testingAutomat";
 $dbPassword = "Automation123#@";
 $dBName = "u611910731_automation";
-
 $conn = mysqli_connect($servername, $dBUsername, $dbPassword, $dBName);
-
 if(!$conn){
-	echo "Databese Connection Failed";
+	echo "Database Connection Failed";
 }
 
 ?>
